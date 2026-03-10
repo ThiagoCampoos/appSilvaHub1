4f2c0ed0cb5267e2300ef61ff17f5d87818404c4
Thiago Campos <thiago.silva.oliveira2001@gmail.com>
2026-03-09 21:18:30 -0300

    feat: criação de pacotes na arquitetura clean e formatação do libs.version.toml

--- composeApp/src/androidMain/kotlin/com/example/silvahub/App.kt
+++ composeApp/src/androidMain/kotlin/com/example/silvahub/App.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.silvahub.ui.components.Greeting
 import org.jetbrains.compose.resources.painterResource
 
 import silvahub.composeapp.generated.resources.Res
